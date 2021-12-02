cd2a13a92d51b0eed5e87e7c309cb801cda1662b
ヒチヒチ <90025466+hclhcl7@users.noreply.github.com>
2021-12-02 12:00:04 +0900

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -4,8 +4,9 @@ canvas.height = window.innerHeight;
 const c = canvas.getContext("2d");
 
 
-let radius = innerHeight * 0.07;
-let h = innerHeight * 0.2;
+const w = innerWidth;
+const h = innerHeight;
+const radius = innerHeight * 0.07;
 let x = innerHeight * 0.1;
 let y = innerHeight * 0.2;
 let dx = innerHeight * 0.01;
@@ -16,17 +17,17 @@ function animate() {
     requestAnimationFrame(animate);
 
     // 全て消す
-    c.clearRect(0, 0, innerWidth, innerHeight);
+    c.clearRect(0, 0, w, h);
 
     // 背景を描く
     c.fillStyle = "rgba(0, 200, 255, 0.5)";
-    c.fillRect(0, 0, innerWidth, innerHeight);
+    c.fillRect(0, 0, w, h);
 
     // 文字を書く
     c.fillStyle = "rgba(0, 0, 0, 1)";
     c.font = "11vw arial";
     c.textAlign = "center";
-    c.fillText("JavaScript canvas", innerWidth / 2, innerHeight / 4);
+    c.fillText("JavaScript canvas", w / 2, h / 4);
 
     // ボールを描く
     c.beginPath();
@@ -36,22 +37,20 @@ function animate() {
 
     // 地面を描く
     c.fillStyle = "rgba(10, 200, 0, 1)";
-    c.fillRect(0, innerHeight - h, innerWidth, h);
+    c.fillRect(0, h * 0.8, w, h * 0.2);
 
     // 変数の更新
     x += dx;
     y += dy;
     dy += 1;
-    radius = innerHeight * 0.07;
-    h = innerHeight * 0.2;
 
     // ボールの跳ね返り(横)
-    if (x + radius > innerWidth || x - radius < 0) {
+    if (x + radius > w || x - radius < 0) {
         dx = -dx;
     }
     
     // ボールの跳ね返り(地面)
-    if (y + radius > innerHeight - h || y - radius < 0) {
+    if (y + radius > h * 0.8 || y - radius < 0) {
         y = y - dy;
         dy = -dy * 0.7;
         n++;
